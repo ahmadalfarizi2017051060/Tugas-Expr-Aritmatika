b8b3cb3708994b4b7aabad76a95064048ce03e21
JoyHansChristabelSinaga <83484843+JoyHansChristabelSinaga@users.noreply.github.com>
2021-07-08 09:31:31 +0700

    Update ExprAritmatika.cpp

--- ExprAritmatika.cpp
+++ ExprAritmatika.cpp
@@ -79,21 +79,85 @@ string IntoPos(string in){
 	
 	string postfix = "";
 	
-	for (i = 0; i < in.length(); i++;){
-		if(in[i] == '-'){
-			if(i == 0){
-				if(isdigit(in[i])){
-					while (i < in.length() && isdigit (in[i])){
+	for(i = 0; i < in.length(); i++){
+		if(isdigit(in[i])){
+			while(i < in.length() && isdigit(in[i])){
+				postfix += in[i];
+				i++;
+			}
+			i--;
+			postfix += " ";
+		}
+		else if(in[i] == '(')
+			data.push(in[i]);
+		else if(in[i] == ')'){
+			while(!data.empty() && data.top() != '('){
+				postfix += data.top();
+				postfix += " ";
+				data.pop();
+			}
+			data.pop();
+		} else {
+			if(in[i] == '-'){
+				if(i==0){
+					if(isdigit(in[i+1])){
 						postfix += in[i];
 						i++;
+						while(i<in.length() && isdigit(in[i])){
+							postfix += in[i];
+							i++;
+						}
+						i--;
+						postfix += " ";
 					}
-					i--;
-					postfix += " ";
+					else{
+						postfix += "-1 ";
+						data.push('*');
+					}
+				} else {
+					while(!data.empty() && precedence(data.top()) >= precedence(in[i]) && !isOp(in[i-1])){
+						postfix += data.top();
+						postfix += " ";
+						data.pop();
+					}
+					
+					if((isdigit(in[i+1]) || in[i+1] =='(') && (isdigit(in[i-1]) || in[i-1] == ')'))
+						data.push(in[i]);
+					else {
+						postfix += "-1 ";
+						data.push('*');
+					}
 				}
-				data.pop();
 			} else {
-				postfix += "-1 ";
-				data.push('*');
-		} else {
-			
+				while(!data.empty() && precedence(data.top()) >= precedence(in[i])){
+					postfix += data.top();
+					postfix += " ";
+					data.pop();
+				}
+				data.push(in[i]);
+			}
+		}
 	}
+	while(!data.empty()){
+		postfix += data.top();
+		postfix += " ";
+		data.pop();
+	}
+	return postfix;
+}
+
+int main(){
+	string input; 
+	string temp = "";
+	
+	getline(cin,input);
+	
+	for(i = 0; i < input.length(); i++){
+		if(input[i] == ' ')
+			continue;
+		else
+			temp += input[i];
+	}
+	cout << IntoPos(temp);
+	return 0;
+}
